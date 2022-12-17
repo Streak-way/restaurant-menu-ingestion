2f0fa96ac16b42d08256bdf159d5cd759db2cbaa
Naveen Jammula <noblenaveen@gmail.com>
2022-12-16 20:38:33 -0800

    login and logout

--- src/App.js
+++ src/App.js
@@ -11,16 +11,28 @@ import {
 import Login from "./login";
 import Sucess from "./Success";
 import RestaurantIntake from "./RestaurantIntake";
-import { useRecoilValue } from "recoil";
 import { loginAtom } from "./recoil/loginAtom";
 import PrivateRoute from "./privateRoute";
 import { Fragment } from "react";
+import { Button } from "@mui/material";
+import supabase from "./supabase";
+import { useRecoilState, useRecoilValue } from "recoil";
 
 function App() {
+  const isLogin = useRecoilValue(loginAtom);
+  const [_, setLogin] = useRecoilState(loginAtom);
+  const handleLogout = async () => {
+    // supabase logout
+    await supabase.auth.signOut();
+    setLogin(false);
+  };
   // https://supabase.com/docs/guides/auth/auth-google
   return (
     <div className="App">
       <header className="App-header">
+        <Button color="primary" onClick={handleLogout}>
+          Logout
+        </Button>
         <Router>
           <Routes>
             <Route path="/" element={<Login />} />
